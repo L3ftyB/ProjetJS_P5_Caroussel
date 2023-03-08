29a51ab207c9e65c7bcda4ccaa4d18259d938b4e
Sidouid HACID <sidouid@gmail.com>
2023-03-08 17:15:24 +0100

    MAJ des commentaires dans JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Tableau qui contient les informations pour chaque slide.
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,32 +18,44 @@ const slides = [
 	}
 ]
 
+// Initialiser la position à 0.
 let position = 0
 
+// Selection des éléments HTML pour afficher l'image et le texte.
 let imageSlide = document.querySelector('.img_slide')
 let texte = document.getElementById('text-slide')
 
+// Selection des éléments HTML pour les bullet points.
 let point = document.getElementsByClassName('dot')
-let nbrPoints = point.length
+const nbrPoints = point.length	// Tableau representant les bulletpoints
 
+// La fonction enleverPoint sert à enlever la classe 'dot_selected' des bullet points.
 function enleverPoint(){
-	for( let i = 0 ; i < nbrPoints; i++){
-		point[i].classList.remove('dot_selected')
+	for( let i = 0 ; i < nbrPoints; i++){    
+		point[i].classList.remove('dot_selected')	// Enlever le point initial suite à l'acrementation
 	}
 }
 
+// Fonction imgSuivante pour afficher la slide suivante.
 function imgSuivante() {
+	// Incrémentation de la position actuelle.
 	position = position + 1
 
+	// Si la position dépasse la longueur du tablau, elle est réinitialisée à 0.
 	if( position > 3){
 		position = 0
 	}
 
+	// Enlever la classe 'dot_selected' du bullet point actuel.
 	enleverPoint()
-	point[position].classList.add('dot_selected')
+	//Ajouter la class 'dot_selected' au bullet point correspondant à la nouvelle position.		
+	point[position].classList.add('dot_selected')      
 	
+	//Selectionner l'objet de la slide correspondant à la nouvelle position.
 	let imageWithTag = slides[position]
+	//mise à jour de l'image affiché avec l'objet correspondant.
 	imageSlide.src = "./assets/images/slideshow/" + imageWithTag.image
+	//mise à jour du texte affiché avec l'objet correpondant.
 	texte.innerHTML = imageWithTag.tagLine
 
 }
@@ -57,18 +70,18 @@ function imgPrecedent() {
 	enleverPoint()
 	point[position].classList.add('dot_selected')
 
-	let imageWithTag = slides[position]
-	imageSlide.src = "./assets/images/slideshow/" + imageWithTag.image
-	texte.innerHTML = imageWithTag.tagLine
+	let imageWithTag = slides[position]                                // On nomme la variable qui regroupe l'image et le texte dans le tableau
+	imageSlide.src = "./assets/images/slideshow/" + imageWithTag.image // On definie la source de l'image dans la variable
+	texte.innerHTML = imageWithTag.tagLine                             // On definie quel texte du tableau on souhaite remplacer
 }
 
-let suivant = document.querySelector('.arrow.arrow_right')
+let suivant = document.querySelector('.arrow.arrow_right')             // On selectionne l'element de la class qu'on souhaite attribuer
 let precedent = document.querySelector('.arrow.arrow_left')
 
-suivant.addEventListener('click', function(){
+suivant.addEventListener('click', function(){                // On attribue un evenement au click pour les fleches suivant et precedents
 	imgSuivante()
 })
-
+					 
 precedent.addEventListener('click', function(){
 	imgPrecedent()
 })
